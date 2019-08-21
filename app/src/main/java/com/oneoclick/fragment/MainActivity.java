53c5d843121c1ae7cc705c522493a9f2eb26c053
chaimard <chaimard@email.krirk.ac.th>
2019-08-21 16:27:05 +0700

    Initial commit

---
+++ app/src/main/java/com/oneoclick/fragment/MainActivity.java
@@ -0,0 +1,19 @@
+package com.oneoclick.fragment;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Fragment fragment = getSupportFragmentManager().findFragmentById(R.id.fragment_my1);
+        MyFragment myFragment = (MyFragment) fragment;
+
+
+    }//void
+}//main
